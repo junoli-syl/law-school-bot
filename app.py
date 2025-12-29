83fb5cd54a2e42a90d36a73e4a0a6ec512cc97b2
junoli-syl <junoli4869@gmail.com>
2025-12-29 01:00:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -68,14 +68,14 @@ st.markdown(
         box-shadow: none !important;
         padding: 0 !important;
         /* 增加顶部间距，让按钮和文字之间有呼吸感 */
-        margin-top: 1 rem !important; 
+        margin-top: 1rem !important; 
     }
 
     /* 专门处理文字样式 */
     [data-testid="stSidebar"] [data-testid="stNotification"] div {
-        font-size: 0.4 rem !important;
+        font-size: 0.7rem !important;
         color: #666 !important;
-        line-height: 0.8 !important;
+        line-height: 1.2 !important;
     }
     </style>
     """, 

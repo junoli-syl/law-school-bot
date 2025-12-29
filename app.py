b68f85f56b7f5f3b55a743f5dc0f93b56faf9b6c
junoli-syl <junoli4869@gmail.com>
2025-12-29 01:43:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ with st.sidebar:
         """
         <div style="
             font-family: 'Times New Roman', Times, serif;
-            font-size: 0.9rem; 
+            font-size: 0.85rem; 
             color: #666; 
             line-height: 1.3; 
             margin-top: 20px;
